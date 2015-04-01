dc2a2bb11747e4f4c64942ca23684f9bd1d21acc
Mike Trinder <mike_github@chthonic.f9.co.uk>
2015-04-01 12:13:27 +0100

    Record opened processes in an array

--- http-process-closer.js
+++ http-process-closer.js
@@ -1,18 +1,34 @@
 var http  = require('http');
 var spawn = require('child_process').spawn;
 var exec  = require('child_process').exec;
+var async = require('async');
 
-var p = spawn(process.argv[2], process.argv.splice(3));
-p.stdout.pipe(process.stdout);
-p.stderr.pipe(process.stderr);
+var p = []; //spawned process arrays
+
+function spawnNewProcess(path, args) {
+  var newProcess = spawn(path, args);
+  newProcess.stdout.pipe(process.stdout);
+  newProcess.stderr.pipe(process.stderr);
+  p.push(newProcess);
+}
+
+//start the first process passed in
+spawnNewProcess(process.argv[2], process.argv.splice(3));
 
 var server = http.createServer(function (req, res) {
   if (req.url === '/close') {
-    exec('taskkill /pid ' + p.pid + ' /T /F', function() {
-      res.statusCode = 200;
-      res.end();
-      process.exit(0);
-    });
+    var tasks = [];
+    p.forEach(function (spawnedProcess) {
+        tasks.push(function(done) {
+           exec('taskkill /pid ' + spawnedProcess.pid + ' /T /F', done);
+         });
+      });
+
+    async.parallel(tasks,  function() {
+        res.statusCode = 200;
+        res.end();
+        process.exit(0);
+      });
     return;
   }
 
